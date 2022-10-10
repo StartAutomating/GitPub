46360818aadffc467fbf005b13ce0842ea646ec5
StartAutomating <StartAutomating@github.com>
2022-10-10 01:56:32 +0000

    Adding Module Scaffolding (Fixes #4)

---
+++ GitPub.psm1
@@ -0,0 +1,7 @@
+foreach ($file in (Get-ChildItem -Path "$psScriptRoot" -Filter "*-*" -Recurse)) {
+    if ($file.Extension -ne '.ps1')      { continue }  # Skip if the extension is not .ps1
+    if ($file.Name -match '\.[^\.]+\.ps1$') { continue }  # Skip if the file is an unrelated file.
+    . $file.FullName
+}
+
+Export-ModuleMember -Function * -Alias *
